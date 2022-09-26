6b0535b70b825c711a44390c567b95164ac8a558
Riefky Maulana <riefky.347@gmail.com>
2022-09-26 15:45:13 +0700

    prepared for deployment

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,5 +1,5 @@
 import PostHeader from "./post-header";
-import  ReactMarkdown  from "React-markdown";
+//import  ReactMarkdown  from "React-markdown";
 import Image from "next/image";
 import {PrismLight as SyntaxHighlighter} from 'react-syntax-highlighter';
 import  atomDark  from "react-syntax-highlighter/dist/cjs/styles/prism/atom-dark";
