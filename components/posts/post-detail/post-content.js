593c87e0b63ac212a4a63a313bfeda31e748c0b8
Riefky Maulana <riefky.347@gmail.com>
2022-09-26 15:43:16 +0700

    prepared for deployment

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -11,44 +11,44 @@ SyntaxHighlighter.registerLanguage('js', js);
 SyntaxHighlighter.registerLanguage('css', css);
 
 function PostContent(props) {
-    const {post} = props;
+    // const {post} = props;
 
-    const imagePath = `/images/posts/${post.slug}/${post.image}`;
+    // const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
-    const customRenderers = {
-        // img: (image) => {
-        //     return <Image src={`${image.src}`} alt={image.alt} width={600} height={300} />
-        // },
-        // p:(paragraph) => {
-        //     const {node} = paragraph;
+    // const customRenderers = {
+    //     // img: (image) => {
+    //     //     return <Image src={`${image.src}`} alt={image.alt} width={600} height={300} />
+    //     // },
+    //     // p:(paragraph) => {
+    //     //     const {node} = paragraph;
 
-        //     if(node.children[0].type === 'image') {
-        //         const image = node.children[0];
+    //     //     if(node.children[0].type === 'image') {
+    //     //         const image = node.children[0];
 
-        //         return (<div className={classes.image}>
-        //             <Image src={`/images/posts/${post.slug}/${image.url}`} alt={image.alt} width={600} height={300} />
-        //         </div>);
-        //     }
-        //     return <p>{paragraph.children}</p>
-        // },
-        code: (code) => {
-            const {className, children} = code;
-            // console.log('print code');
-            // console.log(code)
-            return <SyntaxHighlighter 
-                            style={atomDark}
-                            language={className} 
-                    >
-                        {children}
-                    </SyntaxHighlighter>
-        }
+    //     //         return (<div className={classes.image}>
+    //     //             <Image src={`/images/posts/${post.slug}/${image.url}`} alt={image.alt} width={600} height={300} />
+    //     //         </div>);
+    //     //     }
+    //     //     return <p>{paragraph.children}</p>
+    //     // },
+    //     code: (code) => {
+    //         const {className, children} = code;
+    //         // console.log('print code');
+    //         // console.log(code)
+    //         return <SyntaxHighlighter 
+    //                         style={atomDark}
+    //                         language={className} 
+    //                 >
+    //                     {children}
+    //                 </SyntaxHighlighter>
+    //     }
         
-    };
+    // };
 
-    return <article>
-                <PostHeader title={post.title} image={imagePath} />
-                <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
-            </article>
+    // return <article>
+    //             <PostHeader title={post.title} image={imagePath} />
+    //             <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
+    //         </article>
 }
 
 export default PostContent;
